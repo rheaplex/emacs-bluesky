b661f986196dcc2766254674ec7aff3d7af71834
Andrew Hyatt <ahyatt@gmail.com>
2024-11-30 17:07:59 -0500

    Initial checkin of bluesky connection and ui
    These only establish the patterns, much more is needed to have a functioning app.

---
+++ bluesky-conn.el
@@ -0,0 +1,201 @@
+;;; bluesky-conn.el --- Bluesky API connection functions -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2024  Andrew Hyatt <ahyatt@gmail.com>
+
+;; Author: Andrew Hyatt <ahyatt@gmail.com>
+;; Homepage: https://github.com/ahyatt/emacs-bluesky
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; This file has various methods for calling into bluesky servers according to
+;; the atproto protocol.  We use the atproto objects as is, as json objects that
+;; use plists and arrays.
+
+(require 'json)
+(require 'plz)
+(require 'seq)
+
+;;; Code:
+
+(defvar bluesky-session nil
+  "The session object for accessing the Bluesky API per host and handle.
+This is an alist of host/handle strings and session objects returned by
+the Bluesky API.")
+
+(defun bluesky-conn-json-read ()
+  "Read JSON from the current buffer and return it in as we expect."
+  (let ((json-object-type 'plist))
+    (json-read)))
+
+(defun bluesky-conn-json-read-from-string (str)
+  "Read JSON from STR and return it as we expect."
+  (let ((json-object-type 'plist))
+    (json-read-from-string str)))
+
+(defun bluesky-conn-call (host http-method method auth-header on-success on-error &rest args)
+  "Call METHOD on the Bluesky instance at HOST.
+
+HTTP-METHOD is the HTTP method to use, such as `get' or `post'.
+
+AUTH-HEADER is the value to use for the bearer authorization header; if
+nil it is assumed this is a public endpoint.  ARGS is a plist, but any
+values that are nil will be ignored.
+
+ON-SUCCESS is a function that will be called with the response from the
+Bluesky API. If ON-SUCCESS is nil, the request will be made
+syncronously, and a JSON object will be returned.
+
+ON-ERROR handles all errors."
+  (let* ((url (format "https://%s/xrpc/%s%s" host method
+                      (if (and args (eq 'get http-method))
+                          (concat "?"
+                                  (mapconcat (lambda (pair)
+                                               (format "%s=%s" (url-hexify-string (substring-no-properties (symbol-name (car pair)) 1))
+                                                       (url-hexify-string (format "%s" (cadr pair)))))
+                                             (seq-filter (lambda (pair) (not (null (cadr pair))) args)
+                                                         (seq-partition args 2))
+                                             "&"))
+                        "")))
+         (args (flatten-list (funcall #'append
+                                      (seq-filter (lambda (double)
+                                                    (not (null (cadr double))))
+                                                  (seq-partition args 2)))))
+         (headers (append
+                   (when auth-header `(("Authorization" .
+                                        ,(format "Bearer %s" auth-header))))
+                   '(("Content-Type" . "application/json")))))
+    (apply #'plz http-method url :as #'bluesky-conn-json-read
+           :headers headers
+           (append
+            (when (eq http-method 'post) (list :data (json-encode args)))
+            (when on-success (list :then on-success))
+            (when on-error (list :else
+                                 (when on-error
+                                   (lambda (resp)
+                                     (if-let ((err-resp (plz-error-response resp))
+                                              (err (bluesky-conn-json-read-from-string
+                                                    err-resp)))
+                                         (funcall on-error err)
+                                       (error "No error response found in %s" resp))))))))))
+
+(defun bluesky-conn-call-authed (host handle method on-success on-error &rest args)
+  "Call METHOD on the Bluesky instance at HOST using HANDLE.
+ARGS is a plist, but any values that are nil will be ignored.
+
+ON-SUCCESS is a function that will be called with the response from the
+Bluesky API.
+
+ON-ERROR is a function that will be called with an error JSON object.
+
+This function assumes that a session has been created, and will handle
+auth refreshes."
+  (let ((session (alist-get (format "%s/%s" host handle)
+                            bluesky-session
+                            nil nil #'equal)))
+    (unless session
+      (error "Unable to get the Bluesky authentication token, you may need to log in first."))
+    (bluesky-conn-call host 'get method (plist-get session :accessJwt)
+                       on-success
+                       (lambda (err)
+                         (if (and auth-header (equal "ExpiredToken" (plist-get err :error)))
+                             (progn (bluesky-conn-refresh-session host)
+                                    (apply #'bluesky-conn-call host method auth-header args))
+                           (when on-error (funcall on-error resp))))
+                       args)))
+
+(defun bluesky-conn-create-session (host handle password)
+  "Create a session with the Bluesky API at HOST using HANDLE and PASSWORD.
+HANDLE is the user's handle on the Bluesky instance at HOST (without any
+leading `@'), and PASSWORD is the user's password, or a function that
+takes no arguments that produces it. This function will store the
+session object in `bluesky-session' for future use, and also return it."
+  (let ((result (bluesky-conn-call
+                 host 'post "com.atproto.server.createSession" nil nil nil
+                 :identifier handle :password (if (functionp password)
+                                                  (funcall password)
+                                                password))))
+    (if result
+        (setf (alist-get (format "%s/%s" host handle)
+                         bluesky-session
+                         nil nil #'equal)
+              result)
+      (error "Unable to create a session for host %s" host))))
+
+(defun bluesky-conn-refresh-session (host handle)
+  "Refresh the session for the Bluesky instance at HOST using HANDLE."
+  (let ((session (alist-get (format "%s/%s" host handle)
+                            bluesky-session
+                            nil nil #'equal)))
+    (unless session
+      (error "No session found to refresh for host %s" host))
+    (setf (alist-get (format "%s/%s" host handle)
+                     bluesky-session
+                     nil nil #'equal)
+          (bluesky-conn-call host 'post "com.atproto.server.refreshSession"
+                             (plist-get session :refreshJwt) nil
+                             nil))))
+
+(defun bluesky-conn-create-post (host handle collection record)
+  "Create a post in the Bluesky instance at HOST using HANDLE.
+COLLECTION is the collection to post to, and RECORD is the record, created by
+`bluesky-conn-record', to post."
+  (bluesky-conn-call-authed
+   host handle "com.atproto.server.createPost" nil nil :collection collection :record record))
+
+(defun bluesky-conn-record (text langs facets)
+  (append `(:text ,text :createdAt ,(format-time-string "%Y-%m-%dT%H:%M:%SZ"))
+          (when langs `(:langs ,langs))
+          (when facets `(:facets ,facets))))
+
+(defun bluesky-get-timeline (host handle &optional cursor limit)
+  "Get the timeline for the user HANDLE at HOST.
+The CURSOR defines where to start at, and LIMIT is the number of posts
+to return."
+  (unless (or (null limit) (and (> limit 0) (< limit 100)))
+    (error "Number of posts to retrieve must be between 0 and 100"))
+  (bluesky-conn-call-authed host handle "app.bsky.feed.getTimeline"
+                            nil nil :cursor cursor :limit limit))
+
+(defvar bluesky-conn-cache (make-hash-table :test 'equal)
+  "A cache of Bluesky API responses, keyed by URLs.
+Anything in here is assumed to be cacheable indefinitely.")
+
+(defun bluesky-conn-get-image-by-url (url)
+  "Get an image at URL, using the cache if available."
+  (or (gethash url bluesky-conn-cache)
+      (puthash url (create-image (plz 'get url :as #'binary) nil 'data) bluesky-conn-cache)))
+
+(defun bluesky-conn-get-blob (host did cid)
+  "Get a blob with id CID from account DID."
+  (plz 'get (format "https://%s/xrpc/com.atproto.sync.getBlob?did=%s&cid=%s"
+                    host (url-hexify-string did) (url-hexify-string cid))
+    :as 'binary))
+
+(defun bluesky-conn-get-image-by-ref (host did cid)
+  "Get an image with id CID from account DID."
+  (create-image (bluesky-conn-get-blob host did cid) nil 'data))
+
+;;; Test code
+
+(defun bluesky-conn-test ()
+  (interactive)
+  (let ((host "bsky.social")
+        (auth-info (car (auth-source-search :host bluesky-host))))
+    (bluesky-conn-create-session host (plist-get auth-info :user) (plist-get auth-info :secret))))
+
+(provide 'bluesky-conn)
+
+;;; bluesky-conn.el ends here
---
+++ bluesky-ui.el
@@ -0,0 +1,212 @@
+;;; bluesky-ui.el --- Bluesky UI functions -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2024  Andrew Hyatt <ahyatt@gmail.com>
+
+;; Author: Andrew Hyatt <ahyatt@gmail.com>
+;; Homepage: https://github.com/ahyatt/emacs-bluesky
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; This file renders atproto objects.  Rendering is done by inserting directly
+;; into the buffer.  Overlays are used to both store the underlying objects when
+;; needed, but also to stylize text.
+
+(require 'ewoc)
+(require 'bluesky-conn)
+
+;;; Code:
+
+(defgroup bluesky nil
+  "Bluesky client for Emacs."
+  :group 'applications)
+
+(defcustom bluesky-default-host "https://bluesky.dev"
+  "The default host of the Bluesky server."
+  :type 'string
+  :group 'bluesky)
+
+(defcustom bluesky-image-max-width 250
+  "The maximum width of images in the Bluesky UI."
+  :type 'integer)
+
+(defvar bluesky-host bluesky-default-host
+  "The host of the Bluesky server.")
+
+(defface bluesky-author-name-face
+  '((t :inherit font-lock-keyword-face))
+  "Face for author names in Bluesky.")
+
+(defface bluesky-author-handle-face
+  '((t :inherit font-lock-normal-face))
+  "Face for author handles in Bluesky.")
+
+(defface bluesky-author-attribute-face
+  '((t :inherit font-lock-comment-face))
+  "Face for author attributes in Bluesky.")
+
+(defface bluesky-time-face
+  '((t :inherit font-lock-normal-face))
+  "Face for time in Bluesky UI")
+
+(defface bluesky-mention-face
+  '((t :inherit link))
+  "Face for mentions in Bluesky.")
+
+(defface bluesky-hashtag-face
+  '((t :inherit link))
+  "Face for hashtags in Bluesky.")
+
+(defface bluesky-link-face
+  '((t :inherit link))
+  "Face for links in Bluesky.")
+
+(defun bluesky-ui-render-author (author)
+  "Render AUTHOR to the current buffer"
+  (insert-image (append
+                 (bluesky-conn-get-image (plist-get author :avatar))
+                 '(:width (1 . ch) :height (1 . ch) :margin 2)))
+  (insert
+   (concat
+    (propertize (plist-get author :displayName) 'face 'bluesky-author-name-face)
+    " "
+    (propertize (concat "@" (plist-get author :handle)) 'face 'bluesky-author-handle-face)
+    " "
+    (let* ((viewer (plist-get author :viewer))
+           (muted (not (eq (plist-get viewer :muted) :json-false)))
+           (blocked (not (eq (plist-get viewer :blockedBy) :json-false)))
+           (following (plist-get viewer :following)))
+      (if (or muted blocked following)
+          (propertize
+           (format "[%s]"
+                   (string-join (append
+                                 (when muted '("Muted"))
+                                 (when blocked '("Blocked"))
+                                 (when following '("Following")))
+                                ", "))
+           'face 'bluesky-author-attribute-face)
+        "")))))
+
+(defun bluesky-ui-relative-time (timestr)
+  "Transform TIMESTR to a relative time string for showing users.
+TIMESTR is a string such as 2024-11-29T22:31:30.465Z."
+  (let* ((time (date-to-time timestr))
+         (now (current-time))
+         (diff (time-subtract now time))
+         (diff-seconds (time-to-seconds diff)))
+    (cond
+     ((< diff-seconds 60) "just now")
+     ((< diff-seconds 3600) (format "%d minutes ago" (/ diff-seconds 60)))
+     ((< diff-seconds 86400) (format "%d hours ago" (/ diff-seconds 3600)))
+     ((< diff-seconds 604800) (format "%d days ago" (/ diff-seconds 86400)))
+     (t (format-time-string "%Y-%m-%d" time)))))
+
+(defun bluesky-ui-byte-to-char-offset (text byte-offset)
+  "Convert BYTE-OFFSET to a character offset in TEXT."
+  ;; Take care of the simple case first, to be fast.
+  (if (eq (length text) (string-bytes text))
+      byte-offset
+    ;; We want to find the char offset that corresponds to the byte offset.
+    ;; Doing it linearlly may be too expensive, so let's do a binary search.
+    (unless (< byte-offset (string-bytes text))
+      (error "Byte offset %d is out of range" byte-offset))
+    (let* ((len (length text))
+           (start 0)
+           (end len)
+           (char-offset 0))
+      (while (< (+ 1 start) end)
+        (let ((mid (/ (+ start end) 2)))
+          (if (< (string-bytes (substring text 0 mid)) byte-offset)
+              (setq start mid)
+            (setq end mid))))
+      (+ 1 start))))
+
+(defun bluesky-ui-render-text-with-facets (text facets)
+  "Render TEXT with FACETS to the current buffer.
+The FACETS is a vector of features, with each feature having a type and
+a byte range."
+  (let ((start-pos (point)))
+    (insert text)
+    (dolist (facet (append facets nil))
+      (let* ((features (plist-get facet :features))
+             (index (plist-get facet :index))
+             (start-byte (plist-get index :byteStart))
+             (start-end (plist-get index :byteEnd))
+             ;; We assume that all features have at least one feature in them
+             (feature (aref features 0))
+             (type (cadr (split-string (plist-get feature :$type) "#")))
+             (overlay (make-overlay (+ start-pos
+                                       (bluesky-ui-byte-to-char-offset text start-byte))
+                                    (+ start-pos
+                                       (bluesky-ui-byte-to-char-offset text start-end)))))
+        (overlay-put overlay 'bluesky facet)
+        ;; For the time being, we use only the first feature of the facet, since
+        ;; if there are more than one, it's unclear how to render them.
+        (when (and features (> (length features) 0))
+          (let ((feature (aref features 0)))
+            (overlay-put overlay 'face (pcase type
+                                         ("mention" 'bluesky-mention-face)
+                                         ("hashtag" 'bluesky-hashtag-face)
+                                         ("link" 'bluesky-link-face)))))))))
+
+(defun bluesky-ui-scaled-size (aspect-ratio)
+  "From ASPECT-RATIO, calculate the scaled size of an image.
+The output is suitable for appending to an image object to set the size
+of the image."
+  (let ((width (plist-get aspect-ratio :width))
+        (height (plist-get aspect-ratio :height)))
+    (if (> width bluesky-image-max-width)
+        (let ((scaled-height (/ (* height bluesky-image-max-width) width)))
+          `(:width ,bluesky-image-max-width :height ,scaled-height))
+      `(:width ,width :height ,height))))
+
+(defun bluesky-ui-render-embed (embed author-did)
+  "Render EMBED to the current buffer.
+AUTHOR-DID is the DID of the author of the post, used to fetch content."
+  (let ((images (plist-get embed :images))
+        (video (plist-get embed :video)))
+    (dolist (image (append images nil))
+      (insert-image (append
+                     (bluesky-conn-get-image-by-ref
+                      bluesky-host
+                      author-did
+                      (plist-get
+                       (plist-get (plist-get image :image) :ref)
+                       :$link))
+                     `(:max-width ,bluesky-image-max-width))))))
+
+(defun bluesky-ui-render-record (record author-did)
+  "Render RECORD to the current buffer."
+  (let ((text (plist-get record :text))
+        (facets (plist-get record :facets))
+        (embed (plist-get record :embed)))
+    (bluesky-ui-render-text-with-facets text facets)
+    (insert "\n")
+    (bluesky-ui-render-embed embed author-did)))
+
+(defun bluesky-ui-render-post (post)
+  "Render POST to a string."
+  (bluesky-ui-render-author (plist-get post :author))
+  (let ((record (plist-get post :record))
+        (author-did (plist-get (plist-get post :author) :did)))
+    (insert " "
+            (propertize (bluesky-ui-relative-time
+                         (plist-get record :createdAt))
+                        'face 'bluesky-time-face)
+            "\n")
+    (bluesky-ui-render-record record author-did)))
+
+(provide 'bluesky-ui)
+;;; bluesky-ui.el ends here
